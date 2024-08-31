831e774a5c9e9a012e47adf74f9c8bae836926b5
independencedrive <independencedrive.fr@proton.me>
2024-08-31 17:16:18 +0200

    Update

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,7 +1,113 @@
 function loadFooter() {
   return `
     <section id="dgyxBuh" data-type="section">
-      <!-- Aquí va el resto del contenido del footer -->
+      <style id="NOXXCft" type="text/css" data-type="css" data-size="laptop" data-content="section">
+        grid-container#PxwrHMs {
+          position: relative;
+        }
+        grid-bg#jNVivaa {
+          position: absolute;
+          height: 100%;
+          width: 100%;
+          z-index: -1;
+          display: block;
+        }
+        div#kMbKNTc, div#bDttxoW, div#iiysVnt, div#byCidBs {
+          height: 100%;
+          width: 100%;
+        }
+        div#JvwieGb {
+          height: 100%;
+          width: 100%;
+        }
+        div#OWWFUPQ {
+          height: 100%;
+          width: 100%;
+        }
+        img#BEisfsU {
+          object-fit: cover;
+          height: 100%;
+          width: 100%;
+        }
+        video#fJiQLgU {
+          object-fit: cover;
+          height: 100%;
+          width: 100%;
+        }
+        grid-box#FUgAJeb {
+          display: grid;
+          grid-template-columns: repeat(4, 1fr);
+          grid-auto-rows: auto;
+          width: 100%;
+          height: 100%;
+          min-height: 50px;
+          gap: 1px;
+        }
+        grid-box#FUgAJeb h2 {
+          display: grid;
+          place-content: start;
+          font-size: 24px;
+          color: white;
+          margin-left: 20px;
+          margin-top: 20px;
+          margin-bottom: 20px;
+        }
+        grid-box#FUgAJeb>grid-item {
+          padding: 35px;
+          display: grid;
+          height: 100%;
+          width: 100%;
+          place-items: baseline;
+          background: black;
+        }
+        grid-item#jadynVi {
+          padding: 35px;
+          display: grid;
+          height: 100%;
+          width: 100%;
+          place-items: baseline;
+          place-content: baseline center;
+        }
+        img#oStanlI {
+          object-fit: cover;
+          height: 100%;
+          width: 100%;
+        }
+        #rkqUGeB, #rRToYvo, #UybQYLA, #UyMBjVD {
+          display: grid;
+          gap: 0px;
+        }
+        #rkqUGeB a, #rRToYvo a, #UybQYLA a, #UyMBjVD a {
+          border: none;
+          padding: 5px 20px;
+          color: white;
+          font-size: 14px;
+          border-radius: 10px;
+          text-underline-position: under;
+          height: auto;
+          width: fit-content;
+          display: grid;
+          place-content: center;
+        }
+        a#iReVUOi, a#zLwJfuG, a#QBUJeeR {
+          display: flex;
+          gap: 10px;
+        }
+      </style>
+      <style id="fHtMcds" type="text/css" data-type="css" data-size="tablet" data-content="section">
+        @media screen and (min-width:641px) and (max-width:1024px){
+          grid-box#FUgAJeb {
+            grid-template-columns: repeat(2, 1fr);
+          }
+        }
+      </style>
+      <style id="GqAlgiC" type="text/css" data-type="css" data-size="mobile" data-content="section">
+        @media screen and (max-width:640px){
+          grid-box#FUgAJeb {
+            grid-template-columns: repeat(1, 1fr);
+          }
+        }
+      </style>
       <div id="XOwRsQI" data-type="html">
         <grid-container id="FQAmRAq">
           <grid-bg id="jNVivaa" data-type="grid-bg">
@@ -14,10 +120,73 @@ function loadFooter() {
               <grid-box id="rkqUGeB">
                 <h2 id="nHGqSlL">Contact</h2>
                 <grid-item id="TvwPcFJ">
-                  <a id="RpSIJyb" href="tel:+33664202157" data-type="a-link" target="_self">Tel: 0664202157</a>
+                  <a id="RpSIJyb" href="tel:+33664202157" data-type="a-link" target="_self">Tél : 0664202157</a>
                 </grid-item>
                 <grid-item id="fJMPPAQ">
-                  <a id="fZYKwPC" href="mailto:independencedrive2018@gmail.com" data-type="a-link" target="_self">independencedrive2018@gmail.com</a>
+                  <a id="fZYKwPC" href="#" data-type="a-link" target="_self">independencedrive2018@gmail.com</a>
+                </grid-item>
+                <grid-item id="kxFdtGz">
+                  <a id="lSPTMRn" href="https://www.google.com/maps/search/?api=1&amp;query=83+Rue+Michel+Ange,+34070+Montpellier" data-type="a-link" target="_self">83 Rue Michel Ange, 34070 Montpellier</a>
+                </grid-item>
+                <grid-item id="ObTgPrt">
+                  <a id="iReVUOi" href="https://www.facebook.com/share/5rUeTrLHtpJt3UWt/" data-type="a-link" target="_blank">
+                    <img class="facebook" src="https://raw.githubusercontent.com/independencedrive/site/main/media/images/facebook_50x50.png" alt="Facebook" style="width: 20px; height: 20px;"> Facebook
+                  </a>
+                </grid-item>
+                <grid-item id="pzJgETt">
+                  <a id="QBUJeeR" href="https://www.tiktok.com/@drivingschool_tv?_t=8ojG0WE6Cob&amp;_r=1" data-type="a-link" target="_self">
+                    <img class="tiktok" src="https://raw.githubusercontent.com/independencedrive/site/main/media/images/tiktok_50x50.png" alt="Titktok" style="width: 20px; height: 20px;">Tiktok
+                  </a>
+                </grid-item>
+                <grid-item id="ZptisHS">
+                  <a id="zLwJfuG" href="https://www.instagram.com/independencedrivetv?igsh=MXEwaDJtaW5laW12Mg==" data-type="a-link" target="_self">
+                    <img class="instagram" src="https://raw.githubusercontent.com/independencedrive/site/main/media/images/instagram_50x50.png" alt="Instagram" style="width: 20px; height: 20px;">Instagram
+                  </a>
+                </grid-item>
+              </grid-box>
+            </grid-item>
+            <grid-item id="psnGMAw">
+              <grid-box id="rRToYvo">
+                <h2 id="aBYFfmZ">Prestations</h2>
+                <grid-item id="pltjDLA">
+                  <a id="hPGkeKe" href="./permis-b-boite-manuelle" data-type="a-link" target="_self">Permis B boîte manuelle</a>
+                </grid-item>
+                <grid-item id="TIufMxK">
+                  <a id="eQkcVnB" href="./permis-b-boite-automatique" data-type="a-link" target="_self">Permis B boîte Automatique</a>
+                </grid-item>
+                <grid-item id="kOfZrct">
+                  <a id="IKrvXsc" href="./permis-b-accelere" data-type="a-link" target="_self">Permis B Accéléré </a>
+                </grid-item>
+                <grid-item id="hiBSRqE">
+                  <a id="cmQoVzf" href="./permis-b-heures-illimitees" data-type="a-link" target="_self">Permis B Heures illimitées</a>
+                </grid-item>
+                <grid-item id="OjbYQRM">
+                  <a id="etEgHaW" href="./permis-aac-conduite-accompagnee" data-type="a-link" target="_self">Permis B Conduite Accompagnée (ACC)</a>
+                </grid-item>
+                <grid-item id="kIQzTQU">
+                  <a id="NtLoRtt" href="./permis-b-voyage-disney" data-type="a-link" target="_self">Permis B + Voyage à Disney Paris</a>
+                </grid-item>
+              </grid-box>
+            </grid-item>
+            <grid-item id="FCOGhcT">
+              <grid-box id="UybQYLA">
+                <h2 id="IhSAgYj">Heures d'ouverture</h2>
+                <grid-item id="rYhKeXL">
+                  <a id="IcQEVPv" data-type="a-link" target="_self">lundi au vendredi : 9h - 19h</a>
+                </grid-item>
+                <grid-item id="XBkivDd">
+                  <a id="vVrmzpr" data-type="a-link" target="_self">Samedi : 9h - 12h</a>
+                </grid-item>
+                <grid-item id="ZPaOvUt">
+                  <a id="wyWkjOt" data-type="a-link" target="_self">Dimanche : Fermé</a>
+                </grid-item>
+              </grid-box>
+            </grid-item>
+            <grid-item id="UisZhBL">
+              <grid-box id="UyMBjVD">
+                <h2 id="AsCkclD">Avis</h2>
+                <grid-item id="cfkDoTx">
+                  <a id="zZrvNte" href="./avis-clients" data-type="a-link" target="_self">Avis clients</a>
                 </grid-item>
               </grid-box>
             </grid-item>
@@ -28,4 +197,4 @@ function loadFooter() {
   `;
 }
 
-document.getElementById('footer').innerHTML = loadFooter();
+document.getElementById("footer").innerHTML = loadFooter();
