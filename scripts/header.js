eedb4a91a7dad9de2f76dd42fe3f6d4732d21b76
independencedrive <independencedrive.fr@proton.me>
2024-09-01 20:25:56 +0200

    Update

--- scripts/header.js
+++ scripts/header.js
@@ -1,10 +1,9 @@
 // Define the menu in JSON format with links
 const menuJSON = {
     "items": [
-       
         {
             "name": "Accueil",
-            "url": "./"
+            "url": "/"
         },
         {
             "name": "Prestations",
@@ -12,27 +11,27 @@ const menuJSON = {
             "dropdown": [
                 {
                     "name": "Permis B Boite Manuelle",
-                    "url": "#"
+                    "url": "/permis-b-manuelle"
                 },
                 {
                     "name": "Permis B Boite Automatique",
-                    "url": "#"
+                    "url": "/permis-b-automatique"
                 },
                 {
                     "name": "Permis B Accelere",
-                    "url": "#"
+                    "url": "/permis-b-accelere"
                 },
-                 {
+                {
                     "name": "Permis B Heures illimitees",
-                    "url": "#"
+                    "url": "/permis-b-illimitees"
                 },
-                 {
+                {
                     "name": "Permis B Conduite Accompagnee",
-                    "url": "#"
+                    "url": "/permis-b-conduite-accompagnee"
                 },
-                 {
+                {
                     "name": "Permis B + Voyage a Disney Paris",
-                    "url": "#"
+                    "url": "/permis-b-disney"
                 }
             ]
         },
@@ -42,56 +41,68 @@ const menuJSON = {
             "dropdown": [
                 {
                     "name": "A propos",
-                    "url": "#"
+                    "url": "/a-propos"
                 },
                 {
                     "name": "Contact",
-                    "url": "#"
+                    "url": "/contact"
                 },
                 {
                     "name": "Actualites",
-                    "url": "#"
+                    "url": "/actualites"
                 },
                 {
                     "name": "Avis client",
-                    "url": "#"
+                    "url": "/avis-client"
                 }
             ]
         }
     ]
 };
 
-const baseUrl1 = window.location.origin;
-console.log(baseUrl1); 
-const baseUrl2 = document.baseURI;
-console.log(baseUrl2);
+const baseUrl = document.baseURI;
+console.log(baseUrl);
+
+// Calculate the relative prefix based on the current path
+const currentPath = window.location.pathname;
+const pathDepth = currentPath.split('/').length - 2;
+const prefix = pathDepth > 0 ? '../'.repeat(pathDepth) : '.';
 
 // Function to load the header menu into the section
 function headerMenu(menu) {
     const section = document.getElementById('header');
     let html = '<ul>';
 
-    // Añadir el logo antes del primer elemento del menú
+    // Add the logo before the first menu item
     html += '<li><img class="logo-header" src="https://raw.githubusercontent.com/independencedrive/site/main/media/images/logo_300x172.jpeg" alt="Logo" class="logo" /></li>';
 
     // Iterate over each item in the menu
     menu.items.forEach(item => {
+        let itemUrl = item.url;
+        if (itemUrl.startsWith('/')) {
+            itemUrl = prefix + itemUrl;
+        }
+
         if (item.dropdown) {
             // Dropdown item
             html += `
                 <li class="dropdown">
-                    <a href="${item.url}" class="dropbtn">${item.name}</a>
+                    <a href="${itemUrl}" class="dropbtn">${item.name}</a>
                     <div class="dropdown-content">
-                        ${item.dropdown.map(subItem => `
-                            <a href="${subItem.url}">${subItem.name}</a>
-                        `).join('')}
+                        ${item.dropdown.map(subItem => {
+                            let subItemUrl = subItem.url;
+                            if (subItemUrl.startsWith('/')) {
+                                subItemUrl = prefix + subItemUrl;
+                            }
+                            return `<a href="${subItemUrl}">${subItem.name}</a>`;
+                        }).join('')}
                     </div>
                 </li>
             `;
         } else {
             // Regular item
             html += `
-                <li><a href="${item.url}">${item.name}</a></li>
+                <li><a href="${itemUrl}">${item.name}</a></li>
             `;
         }
     });
