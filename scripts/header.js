a6dfae3b104bcc0b3d8104cc0ba1671aaa146e48
independencedrive <independencedrive.fr@proton.me>
2024-09-20 17:05:24 +0200

    Update

--- scripts/header.js
+++ scripts/header.js
@@ -1,79 +1,9 @@
-const baseUrl = "https://independencedrive.github.io/site";
-const menuJSON = {
-    "items": [
-        {
-            "name": "Accueil",
-            "url": baseUrl,
-        },
-        {
-            "name": "Prestations Permis B",
-            "url": "javascript:void(0)",
-            "dropdown": [
-                {
-                    "name": "Permis B Boite Manuelle",
-                    "url":  baseUrl+"/permis-b-boite-manuelle/"
-                },
-                {
-                    "name": "Permis B Boite Automatique",
-                    "url":  baseUrl+"/permis-b-boite-automatique/"
-                },
-                {
-                    "name": "Permis B Accelere",
-                    "url":  baseUrl+"/permis-b-accelere/"
-                },
-                {
-                    "name": "Permis B Heures illimitees",
-                    "url":  baseUrl+"/permis-b-heures-illimitees/"
-                },
-                {
-                    "name": "Permis B Conduite Accompagnee",
-                    "url":  baseUrl+"/permis-aac-conduite-accompagnee/"
-                },
-                {
-                    "name": "Permis B + Voyage a Disney Paris",
-                    "url":  baseUrl+"/permis-b-voyage-disney/"
-                }
-            ]
-        },
-        {
-            "name": "Prestation Code de la Route",
-            "url": baseUrl+"/preparation-au-code-de-la-route/"
-        },
-        {
-            "name": "Prestation Divers",
-            "url":  baseUrl+"/prestations-diverses/"
-        },
-        {
-            "name": "Infos",
-            "url": "javascript:void(0)",
-            "dropdown": [
-                {
-                    "name": "A propos",
-                    "url":  baseUrl+"/a-propos/"
-                },
-                {
-                    "name": "Contact",
-                    "url":  baseUrl+"/contact/"
-                },
-                {
-                    "name": "Actualites",
-                    "url":  baseUrl+"/actualites/"
-                },
-                {
-                    "name": "Avis clients",
-                    "url":  baseUrl+"/avis-clients/"
-                }
-            ]
-        }
-    ]
-};
-
 function headerMenu(menu) {
     const section = document.getElementById('header');
     let html = '<ul>';
 
     // Logo
-    html += `<li><a href="${baseUrl}" class="logo-link"><img class="logo-header" src="https://raw.githubusercontent.com/independencedrive/site/main/media/images/logo_300x172.jpeg" alt="Logo" class="logo" /></a></li>`;
+    html += `<li><a href = "${baseUrl}" class = "logo-link" ><img class="logo-header" src="https://raw.githubusercontent.com/independencedrive/site/main/media/images/logo_300x172.jpeg" alt="Logo" class="logo" /></a></li>`;
 
     // Iterate over each item in the menu
     menu.items.forEach(item => {
@@ -107,16 +37,6 @@ function headerMenu(menu) {
     html += '</ul>';
     section.innerHTML = html;
 
-    // Add click event to the mobile menu image to toggle the menu
-    const menuIcon = document.getElementById('menu-icon');
-    menuIcon.addEventListener('click', () => {
-        const menuItems = document.querySelectorAll('#header ul li');
-        menuItems.forEach((item, index) => {
-            if (index > 0) { // Skip the logo
-                item.classList.toggle('display-none');
-            }
-        });
-    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
